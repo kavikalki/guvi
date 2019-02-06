95c0fa9a9cdc9e8cc937f454693dd929eaa40677
kavikalki <47381222+kavikalki@users.noreply.github.com>
2019-02-06 01:43:37 -0800

    Create kalki.java

---
+++ kalki.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main(String[] args)throws java.lang.Exception
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("");
+		a=s.nextInt();
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("zero");
+		}
+		else
+		{
+			System.out.println("Negative");
+		
+		}
+	
+	}
+}
+	
+		
+
+	
